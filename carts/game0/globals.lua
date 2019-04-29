8c3c72c37fe85fa6d8011969fcc0a99d6ad7d7b1
jkingsDigiPen <j.kings@digipen.edu>
2019-04-29 00:00:17 -0700

    Update globals to match new map

--- carts/game0/globals.lua
+++ carts/game0/globals.lua
@@ -10,7 +10,9 @@ local globals = {}
 
 globals.pixelsPerUnit = 8 -- Pixels per unit
 globals.viewportWidth = 128 -- Viewport width in pixels
-globals.mapWidth = 23 -- Map width in units
-globals.mapHeight = 13 -- Map height in units
+globals.mapWidth = 32 -- Map width in units
+globals.mapHeight = 16 -- Map height in units
+globals.mapOffsetX = 0 -- Offset of map upper-left in units
+globals.mapOffsetY = 16 -- Offset of map upper-left in units
 
 return globals
