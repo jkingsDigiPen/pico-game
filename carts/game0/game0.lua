5c424e94268f4153e770ea32936612c0ad3bd0ce
jkingsDigiPen <j.kings@digipen.edu>
2019-04-29 00:00:35 -0700

    Add background layers, cursor object

--- carts/game0/game0.lua
+++ carts/game0/game0.lua
@@ -28,11 +28,11 @@ function playerUpdate()
 	end
 	
 	-- jump
-	local jump = -0.8
+	--[[local jump = -0.8
 	if input.isPressed(input.keyCodes.up) and player.grounded then 
 		player.velocity.y += jump
 		player.grounded = false
-	end
+	end]]--
 	
 	-- ANIMATION
 	if player.grounded then
@@ -53,19 +53,52 @@ function playerUpdate()
 		-- play a sound if moving
 		-- (every 8 ticks)
 		if abs(player.velocity.x) + abs(player.velocity.y) 
-				> 0.1 and (player.t % 8) == 0 then
+				> 0.3 and (player.t % 8) == 0 then
 			sfx(1)
 		end
 	end
 end
 
-function cameraUpdate()
-	-- map and actors
-	cam_x = mid(0, player.position.x * globals.pixelsPerUnit - (0.5) * globals.viewportWidth, 
-		(globals.mapWidth - 3) * 3)
+function cursorUpdate()
+	-- MOVEMENT
+	-- How fast to pick up speed
+	local speed = 0.3
+	local direction = vec2:new(0,0)
+	if input.isHeld(input.keyCodes.left) then 
+		direction.x += -1
+	end
+	if input.isHeld(input.keyCodes.right) then 
+		direction.x += 1
+	end
+	if input.isHeld(input.keyCodes.up) then 
+		direction.y += -1
+	end
+	if input.isHeld(input.keyCodes.down) then 
+		direction.y += 1
+	end
+	
+	cursor.velocity = direction:normalized():times(speed)
+	
+	-- ANIMATION
+	if input.isHeld(input.keyCodes.action1) then
+		cursor.frameCurr = 1
+	else
+		cursor.frameCurr = 0
+	end
+	
+	-- Update!
+	cursor:update()
+end
 
-	--cam_y = mid(0,player.y*6-40,128)
-	cam_y = 0
+function cameraUpdate()
+	--[[cam_x = mid(globals.mapOffsetX * 4, player.position.x * globals.pixelsPerUnit - (0.5) * globals.viewportWidth, 
+		(globals.mapWidth + globals.mapOffsetX - 3) * 4)]]--
+  cam_y = mid(globals.mapOffsetY * 4, player.position.y * globals.pixelsPerUnit - (0.5) * globals.viewportWidth, 
+		(globals.mapHeight + globals.mapOffsetY - 3) * 4)
+	
+	-- fixed camera x
+	cam_x = (globals.mapWidth / 2.0) * globals.pixelsPerUnit - (0.5) * globals.viewportWidth
+	
 	camera (cam_x,cam_y)
 end
 
@@ -73,14 +106,28 @@ end
 function _init()
 	input.init()
 
-	-- make player top left
+	-- make player
 	player = gameObject:new
 	{
-		position = vec2:new(6, 2), 
+		position = vec2:new(15, 22), 
 		sprite = 17,
 		bounce = 0,
+		ghostObjects = true,
 	}
 	manager.add(player)
+	
+	-- make cursor
+	cursor = gameObject:new
+	{
+		position = vec2:new(17, 22),
+		sprite = 70,
+		ghostMap = true,
+		ghostObjects = true,
+		gravity = 0,
+	}
+	-- should be drawn on top of foreground, so don't let manager
+	-- update or draw the cursor
+	--manager.add(cursor)
 	
 	-- play level music
 	music(0)
@@ -90,24 +137,46 @@ end
 function guiDraw()
 	-- fix positions of gui elements
 	camera(0, 0)
-	print("x "..player.position.x, 0, 120, 7)
-	print("y "..player.position.y, 64, 120, 7)
-	print("vx "..player.velocity.x, 0, 110, 7)
-	print("vy "..player.velocity.y, 64, 110, 7)
+	print("player_x "..player.position.x, 0, 120, 7)
+	print("player_y "..player.position.y, 64, 120, 7)
+	print("cursor_x "..cursor.position.x, 0, 110, 7)
+	print("cursor_y "..cursor.position.y, 64, 110, 7)
 end
 
 -- Update
 function _update()
 	input.update()
 	playerUpdate()
+	cursorUpdate()
 	manager.update()
-	cameraUpdate()
 end
 
 -- Draw
 function _draw()
 	cls()
-	map(0,0,0,0, globals.mapWidth, globals.mapHeight)
+	
+	-- background color
+	camera(0, 0)
+	rectfill(0,0,127,127,0)
+		
+	-- adjust camera to player
+	cameraUpdate()
+	
+	-- background layer
+	map(globals.mapWidth, 0, 0, globals.mapHeight * globals.pixelsPerUnit, globals.mapWidth, globals.mapHeight)
+	
+	-- object layer
+	map(0, globals.mapHeight, 0, globals.mapHeight * globals.pixelsPerUnit, globals.mapWidth, globals.mapHeight)
+	
+	-- game objects
 	manager.draw()
+	
+	-- foreground layer
+	map(0, 0, 0, globals.mapHeight * globals.pixelsPerUnit, globals.mapWidth, globals.mapHeight)
+	
+	-- cursor object
+	cursor:draw()
+	
+	-- gui
 	guiDraw()
 end
