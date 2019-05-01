03df8f43785eaea9cd7b0a267a346f898cf85aac
jkingsDigiPen <j.kings@digipen.edu>
2019-04-30 18:17:48 -0700

    Update art and map.

--- carts/game0/game0.lua
+++ carts/game0/game0.lua
@@ -206,17 +206,17 @@ function _draw()
 	map(globals.mapWidth, 0, 0, (globals.mapHeight - 0.5) * globals.pixelsPerUnit, globals.mapWidth, globals.mapHeight)
 	
 	-- object layer
-	paletteSetTransparentColor(15) -- beige is now transparent
+	--paletteSetTransparentColor(globals.colors.peach)
 	map(0, globals.mapHeight, 0, globals.mapHeight * globals.pixelsPerUnit, globals.mapWidth, globals.mapHeight)
-	paletteReset() -- default transparency
+	--paletteReset() -- default transparency
 	
 	-- game objects
 	manager.draw()
 	
 	-- foreground layer
-	--paletteSetTransparentColor(15) -- beige is now transparent
+	paletteSetTransparentColor(globals.colors.greenDark)
 	map(0, 0, 0, globals.mapHeight * globals.pixelsPerUnit, globals.mapWidth, globals.mapHeight)
-	--paletteReset() -- default transparency
+	paletteReset() -- default transparency
 	
 	-- cursor object
 	cursor:draw()
--- carts/game0/globals.lua
+++ carts/game0/globals.lua
@@ -8,11 +8,35 @@
 -- Consts
 local globals = {}
 
+-- Viewport/screen
 globals.pixelsPerUnit = 8 -- Pixels per unit
 globals.viewportWidth = 128 -- Viewport width in pixels
+
+-- Map
 globals.mapWidth = 32 -- Map width in units
 globals.mapHeight = 16 -- Map height in units
 globals.mapOffsetX = 0 -- Offset of map upper-left in units
 globals.mapOffsetY = 16 -- Offset of map upper-left in units
+
+-- Colors
+globals.colors =
+{
+	black = 0,
+	blueDark = 1,
+	purpleDark = 2,
+	greenDark = 3,
+	brown = 4,
+	grayDark = 5,
+	grayLight = 6,
+	white = 7,
+	red = 8,
+	orange = 9,
+	yellow = 10,
+	green = 11,
+	blue = 12,
+	purpleLight = 13,
+	pink = 14,
+	peach = 15,
+}
 
 return globals
